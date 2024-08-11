650773d11cb76ae8d23a96e1d16b1cc18cfcd679
Dmitry <kadzzi@gmail.com>
2024-08-11 22:52:12 +0300

    edited module_11_3.py (callable, inspect.ismethod)

--- module_11_3.py
+++ module_11_3.py
@@ -4,16 +4,17 @@ import inspect
 def introspection_info(obj):
     total_info = {}
     total_info['type'] = type(obj).__name__
-    total_info['attributes'] = [x for x in dir(obj) if not callable(x)]
-    total_info['methods'] = [x for x in dir(obj) if callable(x)]
-    total_info['module'] = __name__
+    total_info['attributes'] = [x for x in dir(obj) if not callable(getattr(obj, x))]
+    total_info['methods'] = [x for x in dir(obj) if callable(getattr(obj, x))]
+    total_info['bound_methods'] = [x for x in dir(obj) if inspect.ismethod(getattr(obj, x))]
     if inspect.getmodule(obj) is None:
-        total_info['source'] = '__main__'
+        total_info['module'] = '__main__'
     else:
-        total_info['source'] = inspect.getmodule(obj).__name__
+        total_info['module'] = inspect.getmodule(obj).__name__
 
     return total_info
 
 
-number_info = introspection_info(42)
-print(number_info)
+if __name__ == "__main__":
+    number_info = introspection_info(42)
+    print(number_info)
